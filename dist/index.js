a5479a59cdc49d5ad51698f0bf18bed91f78608f
Andres Moreno <andresmorenojf@gmail.com>
2024-01-19 08:31:46 -0500

    Parameter Properties Shorthand

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,27 @@
 "use strict";
+// class Player {
+// 	// readonly first: string;
+// 	// readonly last: string;
+// 	public readonly first: string;
+// 	public readonly last: string;
+// 	public score = 0
+// 	constructor(first: string, last: string) {
+// 		this.first = first;
+// 		this.last = last;
+// 	}
+// 	private secretMethod(): void {
+// 		console.log('secret message');
+// 	}
+// }
+// const Elton = new Player('Elton', 'Steele')
 class Player {
-    constructor(first, last) {
-        this.score = 0;
+    constructor(first, last, score) {
         this.first = first;
         this.last = last;
+        this.score = score;
     }
     secretMethod() {
         console.log('secret message');
     }
 }
-const Elton = new Player('Elton', 'Steele');
+const Elton = new Player('Elton', 'Steele', 100);
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,28 @@
-class Player {
-	// readonly first: string;
-	// readonly last: string;
-	public readonly first: string;
-	private last: string;
-	public score = 0
+// class Player {
+// 	// readonly first: string;
+// 	// readonly last: string;
+// 	public readonly first: string;
+// 	public readonly last: string;
+// 	public score = 0
 
-	constructor(first: string, last: string) {
-		this.first = first;
-		this.last = last;
-	}
+// 	constructor(first: string, last: string) {
+// 		this.first = first;
+// 		this.last = last;
+// 	}
+
+// 	private secretMethod(): void {
+// 		console.log('secret message');
+// 	}
+// }
+
+// const Elton = new Player('Elton', 'Steele')
+
+class Player {
+	constructor(public first: string, public last: string, private score: number) {}
 
 	private secretMethod(): void {
 		console.log('secret message');
 	}
 }
 
-const Elton = new Player('Elton', 'Steele')
+const Elton = new Player('Elton', 'Steele', 100)
