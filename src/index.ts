25db17791188217d9b5f4bfce44d8fab06b5925c
Andres Moreno <andresmorenojf@gmail.com>
2024-01-19 08:25:21 -0500

    The private Modifier

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ class Player {
 	// readonly first: string;
 	// readonly last: string;
 	public readonly first: string;
-	public readonly last: string;
+	private last: string;
 	public score = 0
 
 	constructor(first: string, last: string) {
